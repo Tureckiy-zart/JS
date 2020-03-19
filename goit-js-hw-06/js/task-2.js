ed07ecbb4219f46070be372f0f27da8f736c112a
Tureckiy <ev.zhdanov@gmail.com>
2020-03-19 23:15:24 +0200

    js-06

---
+++ goit-js-hw-06/js/task-2.js
@@ -0,0 +1,16 @@
+import users from './users.js';
+'use strict';
+
+
+// Получить массив объектов пользователей по цвету глаз 
+// (поле eyeColor). 
+
+// const getUsersWithEyeColor = function(users, color){
+//     return users.filter(user => user.eyeColor === color)
+// }
+
+//===================================СТРЕЛКА=================================================
+const getUsersWithEyeColor = (usersArr, color) => users.filter(user => user.eyeColor === color)
+console.log(getUsersWithEyeColor(users, 'blue'));
+
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
