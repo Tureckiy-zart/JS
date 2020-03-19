0085029c56800c48c21e734d807ddb4acc234c22
Tureckiy <ev.zhdanov@gmail.com>
2020-03-20 01:21:06 +0200

    JS-06-03

---
+++ goit-js-hw-06/js/task-3.js
@@ -0,0 +1,4 @@
+import users from './users.js';
+'use strict';    
+    const getUsersWithGender = (users, genббder) => users.filter(user => user.gender === gen66der);
+    console.log(getUsersWithGender(users, 'male'))
